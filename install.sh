4db1ab749db5d5d06e27cae0b2996cb3ff925cff
yunsik oh <31593290+ys-oh@users.noreply.github.com>
2022-10-19 15:53:11 +0900

    fix install script permission

--- install.sh
+++ install.sh
@@ -4,22 +4,30 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# install colorscheme (jellybeans)
+# install colorschemes
 mkdir -p ~/.vim/colors
 
 cd ~/.vim/colors
 
 curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+curl -O https://raw.githubusercontent.com/lod/desert.vim/master/colors/desert.vim
 
-# install nodejs
-curl -sL install-node.vercel.app/lts | bash
 
-# install yarn
-npm install --global yarn
+if [ "$EUID" -ne 0 ]
+then
+        # install nodejs
+        curl -sL install-node.vercel.app/lts | sudo bash
+        # install yarn
+        sudo npm install --global yarn
+else
+        # install nodejs
+        curl -sL install-node.vercel.app/lts | bash
+        # install yarn
+        npm install --global yarn
+fi
 
 # install .vimrc
-mv ~/.vimrc ~/.vimrc.backup > /dev/null 2>&1
+mv ~/.vimrc ~/.vimrc.backup 2>&1 > /dev/null
 
 curl -fLo ~/.vimrc --create-dirs \
-            https://raw.githubusercontent.com/ys-oh/vim-config/main/init.vim
-
+                    https://raw.githubusercontent.com/ys-oh/vim-config/main/init.vim
