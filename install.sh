8516dfeb251aa769fefc24b81a9a8d4171142cd1
yunsik oh <31593290+ys-oh@users.noreply.github.com>
2022-10-19 15:17:33 +0900

    Update install.sh
    add vimrc install script with previous vimrc backup

--- install.sh
+++ install.sh
@@ -16,3 +16,10 @@ curl -sL install-node.vercel.app/lts | bash
 
 # install yarn
 npm install --global yarn
+
+# install .vimrc
+mv ~/.vimrc ~/.vimrc.backup > /dev/null 2>&1
+
+curl -fLo ~/.vimrc --create-dirs \
+            https://raw.githubusercontent.com/ys-oh/vim-config/main/init.vim
+
